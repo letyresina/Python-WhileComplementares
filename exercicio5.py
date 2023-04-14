b5ebb4f68bce45d5d720c8daa7a42466d313074e
letyresina <leticiaresina@gmail.com>
2023-04-14 11:42:32 -0300

    Criado arquivo exercício 5

---
+++ exercicio5.py
@@ -0,0 +1,6 @@
+'''
+    Exercício 5:
+    Fazer um algoritmo que solicite um número indeterminado de idades de um grupo de indivíduos. 
+    A última idade, que não entrará nos cálculos, contém o valor da idade igual a zero. Calcule a 
+    média de idade deste grupo de indivíduos.
+'''
